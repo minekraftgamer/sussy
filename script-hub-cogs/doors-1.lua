7bfe55badffae6912dee2e3925970fecd1196e87
minekraftgamer <egg694200@tutamail.com>
2024-10-06 17:40:10 +0400

    #1 script

---
+++ script-hub-cogs/doors-1.lua
@@ -0,0 +1,3 @@
+mspaint = loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
+
+return mspaint
--- script-hub.lua
+++ script-hub.lua
@@ -1,9 +1,11 @@
+--[[LIBRARY]]
 
 local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
 
 local Window = Library:CreateWindow({
 
@@ -14,8 +16,51 @@ local Window = Library:CreateWindow({
     MenuFadeTime = 0.2
 })
 
+--[[TABS]]
+
 local Tabs = {
+    
     
     Main = Window:AddTab('Main'),
     ['UI Settings'] = Window:AddTab('UI Settings'),
 }
+
+--[[GROUP BOXES]]
+
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox('DOORS')
+
+--[[UI ELEMENTS]]
+
+LeftGroupBox:AddToggle('DOORS|script', {
+    Text = 'mspaint v2 | DOORS',
+    Default = true, -- Default value (true / false)
+    Tooltip = 'Executes mspaint v2', -- Information shown when you hover over the toggle
+
+    Callback = function(Value)
+        print("mspaint v2 executing")
+    end
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--[[EVENTS]]
+
+Toggles.DOORS|script:OnChanged(function()
+    print("mspaint v2 executed")
+    local mspaint = require("doors-1")
+    print(mspaint)
+end
+
